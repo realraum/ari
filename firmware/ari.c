a170e1e7b857a5001cb1905048510a5a0f94efff
Christian Pointner <equinox@spreadspace.org>
2015-03-10 01:18:03 +0100

    added some simple pwm stuff

--- firmware/ari.c
+++ firmware/ari.c
@@ -32,12 +32,56 @@
 #include "led.h"
 #include "usbio.h"
 
+
+#define PWM_VAL OCR1BL
+
+void pwm_init(void)
+{
+  DDRB |= (1<<PB6);
+  TCCR1B = 0;
+  TCNT1 = 0;
+  OCR1B = 0;
+  TCCR1A = (1<<COM1B1) | (1<<WGM10);
+  TCCR1B = (1<<WGM12);
+}
+
+inline void pwm_on(void)
+{
+  TCCR1B = (TCCR1B & 0xF8) | (1<<CS10);
+}
+
+inline void pwm_off(void)
+{
+  TCCR1B = (TCCR1B & 0xF8);
+  TCNT1 = 0;
+}
+
+inline void pwm_set(uint8_t val)
+{
+  PWM_VAL = val;
+}
+
+inline void pwm_inc(void)
+{
+  if(PWM_VAL < 255)
+    PWM_VAL++;
+}
+
+inline void pwm_dec(void)
+{
+  if(PWM_VAL > 0)
+    PWM_VAL--;
+}
+
+
 void handle_cmd(uint8_t cmd)
 {
   switch(cmd) {
   case '0': led_off(); break;
   case '1': led_on(); break;
   case 't': led_toggle(); break;
+  case '+': pwm_inc(); printf("pwm = %d\r\n", PWM_VAL);  break;
+  case '-': pwm_dec(); printf("pwm = %d\r\n", PWM_VAL); break;
   case 'r': reset2bootloader(); break;
   default: printf("error\r\n"); return;
   }
@@ -52,6 +96,7 @@ int main(void)
   cpu_init();
   led_init();
   usbio_init();
+  pwm_init();
   sei();
 
   for(;;) {
