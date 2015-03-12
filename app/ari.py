5e7ca67c64d48535a6765f170ad127d0717e4a4b
Christian Pointner <equinox@spreadspace.org>
2015-03-12 05:17:56 +0100

    refactoring...

--- app/ari.py
+++ app/ari.py
@@ -30,19 +30,9 @@ import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GObject
 
-def lvl_clamp(x):
-    if x < -90.0:
-        return -90.0
-    elif x > 0.0:
-        return 0.0
-    return x
-
-def lvl_conv(x):
-    x += 90.0
-    return x/90.0
 
 class R3Ari():
-    def __init__(self):
+    def __init__(self, width=1280, height=720):
         GObject.threads_init()
         Gst.init(None)
         GObject.set_prgname("r3 audience response indicator")
@@ -50,8 +40,8 @@ class R3Ari():
         self.pipeline_ = None
         self.watch_id_ = None
 
-        self.video_width_ = 1280
-        self.video_height_ = 720
+        self.video_width_ = width
+        self.video_height_ = height
         self.meter_width_ = 0.75*self.video_width_
         self.meter_height_ = 0.03*self.video_height_
         self.meter_spacing_ =  0.4*self.meter_height_
@@ -86,86 +76,95 @@ class R3Ari():
             s = message.get_structure()
             if s.get_name() == 'level':
                 sys.stdout.write("\r")
-                l = lvl_clamp(s['peak'][0])
-                lp = lvl_clamp(s['decay'][0])
-                r = lvl_clamp(s['peak'][1])
-                rp = lvl_clamp(s['decay'][1])
+                l = self.lvl_clamp(s['peak'][0])
+                lp = self.lvl_clamp(s['decay'][0])
+                r = self.lvl_clamp(s['peak'][1])
+                rp = self.lvl_clamp(s['decay'][1])
                 sys.stdout.write("left: %3.2f / %3.2f, right: %3.2f / %3.2f  " % (l, lp, r, rp))
                 sys.stdout.flush()
-                self.updateMeter(lvl_conv(l), lvl_conv(lp), lvl_conv(r), lvl_conv(rp))
+                self.updateMeter(self.lvl_conv(l), self.lvl_conv(lp), self.lvl_conv(r), self.lvl_conv(rp))
 
         return True
+
+    def create_video_pipeline(self):
+        q_vin = Gst.ElementFactory.make("queue")
+        self.pipeline_.add(q_vin)
+        conv_vin = Gst.ElementFactory.make("videoconvert")
+        self.pipeline_.add(conv_vin)
+
+        scale_vin = Gst.ElementFactory.make("videoscale")
+        self.pipeline_.add(scale_vin)
+        cf_vin = Gst.ElementFactory.make("capsfilter")
+        caps_vin = Gst.Caps.from_string("video/x-raw,width=%i,height=%i" % (self.video_width_, self.video_height_))
+        cf_vin.set_property("caps", caps_vin)
+        self.pipeline_.add(cf_vin)
+
+        q_vscaled = Gst.ElementFactory.make("queue")
+        self.pipeline_.add(q_vscaled)
+        conv_vscaled = Gst.ElementFactory.make("videoconvert")
+        self.pipeline_.add(conv_vscaled)
+
+        self.overlay_ = Gst.ElementFactory.make("rsvgoverlay")
+        self.overlay_.set_property("data", self.getVumeterSVG(0, 0, 0, 0))
+        self.pipeline_.add(self.overlay_)
+
+        conv_vout = Gst.ElementFactory.make("videoconvert")
+        self.pipeline_.add(conv_vout)
+        vsink = Gst.ElementFactory.make("xvimagesink")
+        self.pipeline_.add(vsink)
+
+
+        q_vin.link(conv_vin)
+        conv_vin.link(scale_vin)
+        scale_vin.link(cf_vin)
+        cf_vin.link(q_vscaled)
+        q_vscaled.link(conv_vscaled)
+        conv_vscaled.link(self.overlay_)
+        self.overlay_.link(conv_vout)
+        conv_vout.link(vsink)
+
+        return q_vin.get_static_pad("sink")
+
+
+    def create_audio_pipeline(self):
+        q_ain = Gst.ElementFactory.make("queue")
+        self.pipeline_.add(q_ain)
+        conv_ain = Gst.ElementFactory.make("audioconvert")
+        self.pipeline_.add(conv_ain)
+
+        level = Gst.ElementFactory.make("level", "level")
+        level.set_property("message", True)
+        level.set_property("peak-ttl", self.lvl_pkttl_)
+        level.set_property("peak-falloff", self.lvl_pkfalloff_)
+        self.pipeline_.add(level)
+
+        asink = Gst.ElementFactory.make("fakesink")
+        self.pipeline_.add(asink)
+
+
+        q_ain.link(conv_ain)
+        conv_ain.link(level)
+        level.link(asink)
+
+        return q_ain.get_static_pad("sink")
+
 
     def run(self):
         try:
             self.pipeline_ = Gst.Pipeline.new()
 
-	    source = "tcpclientsrc host=localhost port=1234 ! queue ! gdpdepay"
+            source = "tcpclientsrc host=localhost port=1234 ! queue ! gdpdepay"
             source_bin = Gst.parse_bin_from_description(source, "source")
             self.pipeline_.add(source_bin)
             decoder = Gst.ElementFactory.make("decodebin")
             self.pipeline_.add(decoder)
 
-
-            q_vin = Gst.ElementFactory.make("queue")
-            self.pipeline_.add(q_vin)
-            conv_vin = Gst.ElementFactory.make("videoconvert")
-            self.pipeline_.add(conv_vin)
-
-            scale_vin = Gst.ElementFactory.make("videoscale")
-            self.pipeline_.add(scale_vin)
-            cf_vin = Gst.ElementFactory.make("capsfilter")
-            caps_vin = Gst.Caps.from_string("video/x-raw,width=%i,height=%i" % (self.video_width_, self.video_height_))
-            cf_vin.set_property("caps", caps_vin)
-            self.pipeline_.add(cf_vin)
-
-            q_vscaled = Gst.ElementFactory.make("queue")
-            self.pipeline_.add(q_vscaled)
-            conv_vscaled = Gst.ElementFactory.make("videoconvert")
-            self.pipeline_.add(conv_vscaled)
-            self.overlay_ = Gst.ElementFactory.make("rsvgoverlay")
-            self.overlay_.set_property("data", self.getVumeterSVG(0, 0, 0, 0))
-            self.pipeline_.add(self.overlay_)
-            conv_vout = Gst.ElementFactory.make("videoconvert")
-
-            self.pipeline_.add(conv_vout)
-            vsink = Gst.ElementFactory.make("xvimagesink")
-            self.pipeline_.add(vsink)
-
-
-            q_vin.link(conv_vin)
-            conv_vin.link(scale_vin)
-            scale_vin.link(cf_vin)
-            cf_vin.link(q_vscaled)
-            q_vscaled.link(conv_vscaled)
-            conv_vscaled.link(self.overlay_)
-            self.overlay_.link(conv_vout)
-            conv_vout.link(vsink)
-
-
-
-            q_ain = Gst.ElementFactory.make("queue")
-            self.pipeline_.add(q_ain)
-            conv_ain = Gst.ElementFactory.make("audioconvert")
-            self.pipeline_.add(conv_ain)
-            level = Gst.ElementFactory.make("level", "level")
-            level.set_property("message", True)
-            level.set_property("peak-ttl", self.lvl_pkttl_)
-            level.set_property("peak-falloff", self.lvl_pkfalloff_)
-            self.pipeline_.add(level)
-            asink = Gst.ElementFactory.make("fakesink")
-            self.pipeline_.add(asink)
-
-            q_ain.link(conv_ain)
-            conv_ain.link(level)
-            level.link(asink)
-
+            vin_pad = self.create_video_pipeline()
+            ain_pad = self.create_audio_pipeline()
 
             source_bin.link(decoder)
-            sink_pads = {"video": q_vin.get_static_pad("sink"),
-                         "audio": q_ain.get_static_pad("sink")}
+            sink_pads = {"video": vin_pad, "audio": ain_pad}
             decoder.connect("pad-added", self.decoder_callback, sink_pads)
-
 
             self.pipeline_.get_bus().add_signal_watch()
             self.watch_id_ = self.pipeline_.get_bus().connect('message', self.on_message)
@@ -191,6 +190,18 @@ class R3Ari():
 
             if sinkpad.can_link(srcpad):
                 srcpad.link(sinkpad)
+
+
+    def lvl_clamp(self, x):
+        if x < -90.0:
+            return -90.0
+        elif x > 0.0:
+            return 0.0
+        return x
+
+    def lvl_conv(self, x):
+        x += 90.0
+        return x/90.0
 
 
     def getVumeterSVG(self, l, lp, r, rp):
